1a21ee1594b546fb44da7d8a78273987022963e3
luddovitz <ludwig.reisaronsson@gmail.com>
2024-03-20 14:20:45 +0100

    Updated gradle with maven publish

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,12 +1,33 @@
 plugins {
     id("java")
+    `maven-publish`
 }
 
 group = "com.ludwigreis"
 version = "1.0-SNAPSHOT"
 
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.ludwigreis"
+            artifactId = "jsieparser"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
+}
+
 repositories {
     mavenCentral()
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/luddovitz/jsieparser")
+        credentials {
+            username = System.getenv("GITHUB_ACTOR")
+            password = System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 dependencies {
