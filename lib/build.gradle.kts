d3ada9ec56943f43e6a193e6d0194a1756ae2ab1
luddovitz <ludwig.reisaronsson@gmail.com>
2024-03-20 16:38:54 +0100

    updated workflow

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,7 +9,7 @@ version = "1.0"
 publishing {
     publications {
         register<MavenPublication>("maven") {
-            groupId = "com.ludwigreis"
+            groupId = "luddovitz"
             artifactId = "jsieparser"
             version = "1.0"
 
