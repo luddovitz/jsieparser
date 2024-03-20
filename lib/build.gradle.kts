f5b872d51da15a5b8ac76e0732c7b758a41306ab
luddovitz <ludwig.reisaronsson@gmail.com>
2024-03-20 16:38:11 +0100

    updated workflow

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -31,8 +31,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit:junit-bom:5.9.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
